a51855614f48780bd70386cc652ba95ac524f4a9
Eric West <esw9999@gmail.com>
2015-09-15 14:45:59 -0500

    Add lib/micro_kanren.ex

--- lib/micro_kanren.ex
+++ lib/micro_kanren.ex
@@ -1,2 +1,13 @@
 defmodule MK do
+
+  def lvar([c]), do: HashSet.new |> HashSet.put(c)
+  def lvar?([x]), do: x.__struct__ == HashSet
+
+  def walk(u, s) do
+    cond do
+      lvar?(u) && Set.member?(s, u) ->
+        
+    end
+  end
+
 end
